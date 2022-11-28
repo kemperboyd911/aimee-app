6606ae74ab007d7cefc808e3612d2d8d079133ec
Isal <58117968+rfzsal@users.noreply.github.com>
2022-11-28 10:38:15 +0700

    Update Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,213 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  setStatusBarBackgroundColor,
+  setStatusBarStyle,
+} from 'expo-status-bar';
+import { useEffect } from 'react';
+import { View, StyleSheet, SectionList } from 'react-native';
+import { TextInput, useTheme } from 'react-native-paper';
 
+import posts from '../../_DATA/posts.json';
+import Divider from '../../components/Divider';
+import HorizontalSection from '../../components/HorizontalSection';
 import SafeAreaView from '../../components/SafeAreaView';
+import VerticalSection from '../../components/VerticalSection';
+import Container from '../../layout/Container';
+import BannerCarousel from '../../views/Home/BannerCarousel';
+import HorizontalMenu from '../../views/Home/HorizontalMenu';
+
+const MENU = [
+  {
+    id: '1',
+    title: 'Program',
+    link: '',
+    icon: 'chip',
+  },
+  {
+    id: '2',
+    title: 'Mentor',
+    link: '',
+    icon: 'account-tie',
+  },
+  {
+    id: '3',
+    title: 'Startup',
+    link: '',
+    icon: 'rocket',
+  },
+  {
+    id: '4',
+    title: 'Matchmaking',
+    link: '',
+    icon: 'puzzle',
+  },
+];
+
+const BANNER = [
+  {
+    id: '1',
+    title: '',
+    link: '',
+    cover: 'https://i.postimg.cc/HkQRDTTY/IMG-20221122-WA0102.jpg',
+  },
+  {
+    id: '2',
+    title: '',
+    link: '',
+    cover: 'https://i.postimg.cc/QdLwyFGY/AIMEE-with-P-Sandy-Kosasi-1.jpg',
+  },
+  {
+    id: '3',
+    title: '',
+    link: '',
+    cover: 'https://i.postimg.cc/HWhfbSSg/IMG-20221122-WA0109.jpg',
+  },
+  {
+    id: '4',
+    title: '',
+    link: '',
+    cover: 'https://i.postimg.cc/g0m5G10v/IMG-20221122-WA0110.jpg',
+  },
+];
+
+const POSTS = posts.map((post) => {
+  return {
+    id: String(post.id),
+    title: post.title.rendered,
+    cover: post.yoast_head_json.og_image[0].url,
+    link: post.link,
+  };
+});
+
+const SECTIONS = [
+  {
+    title: 'TopBox',
+    data: [],
+  },
+  {
+    title: 'Search',
+    data: [],
+  },
+  {
+    title: 'Banner',
+    data: BANNER,
+  },
+  {
+    title: 'Menu',
+    data: MENU,
+  },
+  {
+    title: 'Divider',
+    data: [],
+  },
+  {
+    title: 'Article for You',
+    data: POSTS.slice(15, 20),
+  },
+  {
+    title: 'Blog and News',
+    data: POSTS.slice(30, 35),
+  },
+];
+
+const TopBox = () => {
+  const { colors } = useTheme();
+  return (
+    <>
+      <View
+        style={[
+          styles.topPart,
+          {
+            backgroundColor: colors.primary,
+          },
+        ]}
+      />
+      <View
+        style={[
+          styles.bottomPart,
+          {
+            backgroundColor: colors.surface,
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+const Search = () => {
+  const { colors } = useTheme();
+  return (
+    <Container>
+      <TextInput
+        placeholder="Search activity..."
+        left={
+          <TextInput.Icon
+            name="magnify"
+            color={colors.disabled}
+            rippleColor={colors.background}
+            style={styles.searchIcon}
+          />
+        }
+        mode="outlined"
+        style={{ backgroundColor: colors.surface }}
+      />
+    </Container>
+  );
+};
 
 const Home = () => {
+  const { colors } = useTheme();
+
+  const handleScroll = (e) => {
+    if (e.nativeEvent.contentOffset.y < 36) {
+      setStatusBarBackgroundColor(colors.primary);
+      setStatusBarStyle('light');
+    }
+
+    if (e.nativeEvent.contentOffset.y >= 36) {
+      setStatusBarBackgroundColor(colors.surface);
+      setStatusBarStyle('dark');
+    }
+  };
+
+  useEffect(() => {
+    setStatusBarBackgroundColor(colors.primary);
+    setStatusBarStyle('light');
+  }, []);
+
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Text>Home</Text>
-      </View>
+      <SectionList
+        sections={SECTIONS}
+        renderSectionHeader={({ section }) => {
+          if (section.title === 'TopBox') return <TopBox />;
+
+          if (section.title === 'Search') return <Search />;
+
+          if (section.title === 'Banner')
+            return <BannerCarousel data={section.data} mt={16} />;
+
+          if (section.title === 'Menu')
+            return (
+              <Container mt={24}>
+                <HorizontalMenu data={section.data} />
+              </Container>
+            );
+
+          if (section.title === 'Divider') return <Divider />;
+
+          if (section.title === 'Blog and News')
+            return (
+              <VerticalSection title={section.title} data={section.data} />
+            );
+
+          return (
+            <HorizontalSection title={section.title} data={section.data} />
+          );
+        }}
+        renderItem={() => null}
+        onScroll={handleScroll}
+        keyExtractor={(item, index) => String(item + index)}
+      />
     </SafeAreaView>
   );
 };
@@ -15,12 +215,13 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-  superContainer: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  searchIcon: { top: 2 },
+  topPart: { height: 48 },
+  bottomPart: {
+    height: 16,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    bottom: 16,
+    marginBottom: -56,
   },
 });
