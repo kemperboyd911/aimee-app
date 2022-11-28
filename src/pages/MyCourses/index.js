8c9175c0b872f2ece1618276be418fdbb487427b
Isal <58117968+rfzsal@users.noreply.github.com>
2022-11-28 11:50:12 +0700

    Update My Course page

--- src/pages/MyCourses/index.js
+++ src/pages/MyCourses/index.js
@@ -3,12 +3,64 @@ import {
   setStatusBarStyle,
 } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { StyleSheet, SectionList } from 'react-native';
+import { TextInput, useTheme } from 'react-native-paper';
 
+import posts from '../../_DATA/posts.json';
+import HorizontalSection from '../../components/HorizontalSection';
 import SafeAreaView from '../../components/SafeAreaView';
+import VerticalSection from '../../components/VerticalSection';
+import Container from '../../layout/Container';
+import CategorySlider from '../../views/Explore/CategorySlider';
 
-const MyCourses = ({ navigation }) => {
+const POSTS = posts.map((post) => {
+  return {
+    id: String(post.id),
+    title: post.title.rendered,
+    cover: post.yoast_head_json.og_image[0].url,
+    link: post.link,
+  };
+});
+
+const SECTIONS = [
+  {
+    title: 'Search',
+    data: [],
+  },
+  {
+    title: 'History',
+    horizontal: true,
+    data: POSTS.slice(19, 22),
+  },
+  {
+    title: 'My Courses',
+    horizontal: true,
+    data: POSTS.slice(18, 23),
+  },
+];
+
+const Search = () => {
+  const { colors } = useTheme();
+  return (
+    <Container mt={8} mb={16}>
+      <TextInput
+        placeholder="Search activity..."
+        left={
+          <TextInput.Icon
+            name="magnify"
+            color={colors.disabled}
+            rippleColor={colors.background}
+            style={styles.searchIcon}
+          />
+        }
+        mode="outlined"
+        style={{ backgroundColor: colors.surface }}
+      />
+    </Container>
+  );
+};
+
+const Explore = ({ navigation }) => {
   const { colors } = useTheme();
 
   useEffect(() => {
@@ -19,25 +71,31 @@ const MyCourses = ({ navigation }) => {
 
     return unsubscribe;
   }, [navigation]);
-
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Text>My Courses</Text>
-      </View>
+      <SectionList
+        sections={SECTIONS}
+        renderSectionHeader={({ section }) => {
+          if (section.title === 'Search') return <Search />;
+
+          if (section.title === 'Category')
+            return <CategorySlider data={section.data} />;
+
+          if (section.title === 'History')
+            return (
+              <HorizontalSection title={section.title} data={section.data} />
+            );
+
+          return <VerticalSection title={section.title} data={section.data} />;
+        }}
+        renderItem={() => null}
+        keyExtractor={(item, index) => String(item + index)}
+      />
     </SafeAreaView>
   );
 };
-
-export default MyCourses;
+export default Explore;
 
 const styles = StyleSheet.create({
-  superContainer: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  searchIcon: { top: 2 },
 });
